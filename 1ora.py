ee796c460804ab7dbb8fccdcc4dfca748b05ae72
Logicsabi <logicsabi@gmail.com>
2025-02-11 17:40:01 +0100

    Initial Commit

---
+++ 1ora.py
@@ -0,0 +1,29 @@
+"""
+print("Hello World!") # = comment, f5 to start
+név = "Adam"
+print("Hello " + név + "!")
+
+myName = inout("Add meg a neved: ")
+print("Hello " + myName + "!")
+print(type(myName))
+print(id(myName))
+print(id(név))
+"""
+
+name = "Logi Róbert"
+age = 11
+weight = 46.7
+smart = True
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(smart))
+
+print("Szia! " + name + " vagyok.")
+print("Idén vagyok " + str(age) + " éves.")
+print(str(weight) + " kg a súlyom.")
+if smart:
+    print("Okos vagyok.")
+else:
+    print("Nem vagyok valami okos.")
