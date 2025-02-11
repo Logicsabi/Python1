6ec9df21bf324753b9a5cda47a1dcb41ceb94501
Logicsabi <logicsabi@gmail.com>
2025-02-11 18:02:53 +0100

    len, split

---
+++ 2ora.py
@@ -0,0 +1,9 @@
+szöveg = "A fekete cica átsétált az úton, hogy megegye at ott találhati párizsit."
+print(szöveg)
+print("A szöveg hossza:", len(szöveg))
+
+szöveg_szavanként = szöveg.split(" ")
+print(szöveg_szavanként)
+
+szöveg_azként = szöveg.split("az")
+print(szöveg_azként)
