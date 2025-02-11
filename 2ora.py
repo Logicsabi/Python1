5c0fc23f1df2a4dc7a48cd0924e90827581ee352
Logicsabi <logicsabi@gmail.com>
2025-02-11 18:21:56 +0100

    starts/ends with

--- 2ora.py
+++ 2ora.py
@@ -7,3 +7,14 @@ print(szöveg_szavanként)
 
 szöveg_azként = szöveg.split("az")
 print(szöveg_azként)
+első_n = szöveg.find("n")
+print("Az 'n' karakter első előfordulásának helye:", első_n)
+
+első_az = szöveg.find("az")
+print("Az 'az' karakter első előfordulásának helye:", első_az)
+
+print(szöveg_startswith("A"))
+print(szöveg_startswith("x"))
+print(szöveg_endswith("."))
+print(szöveg_endswith("k"))
+
