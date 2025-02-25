46d2fdc58f3c012832e536bb2a57bfbfd1193a90
Logicsabi <logicsabi@gmail.com>
2025-02-25 17:45:15 +0100

    lista

--- 4ora.py
+++ 4ora.py
@@ -1,3 +1,14 @@
 i = 1
 while i <= 10:
-    print(i)
+    print(i, end = " ")
+    i += 1
+print()
+
+lista = []
+bemenet = "default"
+while bemenet != "":
+    bemenet = input("Add meg a lista következő elemét!")
+    if bemenet != "":
+        lista.append(bemenet)
+
+print(lista)
