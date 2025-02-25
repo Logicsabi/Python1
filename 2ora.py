d4deb4b70a44e72438a4ad6cffcab64c1e100678
Logicsabi <logicsabi@gmail.com>
2025-02-25 17:39:04 +0100

    message

--- 2ora.py
+++ 2ora.py
@@ -1,4 +1,4 @@
-szöveg = "A fekete cica átsétált az úton, hogy megegye at ott találhati párizsit."
+szöveg = "A fekete cica átsétált az úton, hogy megegye az ott találhati párizsit."
 print(szöveg)
 print("A szöveg hossza:", len(szöveg))
 
@@ -7,16 +7,17 @@ print(szöveg_szavanként)
 
 szöveg_azként = szöveg.split("az")
 print(szöveg_azként)
+
 első_n = szöveg.find("n")
 print("Az 'n' karakter első előfordulásának helye:", első_n)
 
 első_az = szöveg.find("az")
 print("Az 'az' karakter első előfordulásának helye:", első_az)
 
-print(szöveg_startswith("A"))
-print(szöveg_startswith("x"))
-print(szöveg_endswith("."))
-print(szöveg_endswith("k"))
+print(szöveg.startswith("A"))
+print(szöveg.startswith("x"))
+print(szöveg.endswith("."))
+print(szöveg.endswith("k"))
 
 szó = "kUtYa"
 print(szó)
@@ -25,3 +26,28 @@ print(szó.lower())      # kutya
 print(szó.upper())      # KUTYA
 
 print("a cica története".title()) # Minden szót nagybetűvel kezd
+
+print(szöveg.lower().count("a"))
+
+
+
+thing_1 = input("Type an object:\n")
+thing_2 = input("Type another object:\n")
+adjective = input("Give me an adjective:\n")
+song_name = input("Give me the name of a song:\n")
+celebrity = input("Give me a celebrity name:\n")
+feeling = input("Give me a feeling:\n")
+verb = input("Give me a verb:\n")
+place = input("Give me a place:\n")
+food = input("Give me a food item:\n")
+person = input("Give me a person/name:\n")
+
+print(f"""
+    I just got back from a pizza party with {person}.
+    Can you believe we got to eat {adjective} pizza in {place}?!
+    Everyone got to choose their own toppings.
+    I made '{food} and {thing_1}' pizza, which is my favorite!
+    They even stuffed the crust with {thing_2}. How {feeling}!
+    If that wasn't good enough already, {celebrity} was there singing {song_name}.
+    I was so inspired by the music, I had to get up out of my seat and {verb}.
+    """)
