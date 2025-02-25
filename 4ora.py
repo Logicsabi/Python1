b578f63ef0f16f335f4e726310fbd4dadebbe22e
Logicsabi <logicsabi@gmail.com>
2025-02-25 17:49:21 +0100

    for basic

--- 4ora.py
+++ 4ora.py
@@ -6,9 +6,18 @@ print()
 
 lista = []
 bemenet = "default"
+bemenet = " "
 while bemenet != "":
     bemenet = input("Add meg a lista következő elemét!")
     if bemenet != "":
         lista.append(bemenet)
 
 print(lista)
+
+for i in range(10):
+    print(i, end= " ")
+print()
+
+for i in range(5, 15):
+    print(i, end= " ")
+print()
