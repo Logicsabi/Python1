84fecf6207e05a9d5584bbaa4258611481ff0671
Logicsabi <logicsabi@gmail.com>
2025-02-25 18:54:21 +0100

    min, max, sum, round, range

--- 4ora.py
+++ 4ora.py
@@ -46,3 +46,57 @@ for item in lista:
     összeg += item
 print("A lista elemeinek az összege:", összeg)
 
+"""2."""
+
+lista = [11, -4, 7, 1, 6, -5, -12, 0, 11, 14, -8, -9, 2, 23, 11, 10, -4, 0, 18, 4, 8, 1, 1, 11]
+print(lista)
+min(lista)
+for item in lista:
+    min = item
+print("A lista legkisebb eleme:")
+
+lista = [11, -4, 7, 1, 6, -5, -12, 0, 11, 14, -8, -9, 2, 23, 11, 10, -4, 0, 18, 4, 8, 1, 1, 11]
+print(lista)
+összeg = 0
+for item in lista:
+    if item > 0:
+        összeg += item
+print("A lista pozitív elemeinek az összege:", összeg)
+
+lista = [11, -4, 7, 1, 6, -5, -12, 0, 11, 14, -8, -9, 2, 23, 11, 10, -4, 0, 18, 4, 8, 1, 1, 11]
+print(lista)
+összeg = 0
+for item in lista:
+    if item < 0:
+        összeg += item
+print("A lista negatív elemeinek az összege:", összeg)
+
+max_index = 0
+for i in range(len(lista)):
+    if lista[i] > lista[max_index]:
+        max_index = i
+print("A legnagyobb szám:", lista[max_index])
+
+min_index = 0
+for i in range(len(lista)):
+    if lista[i] < lista[min_index]:
+        min_index = i
+print("A legkisebb szám:", lista[min_index])
+
+számláló = 0
+for item in lista:
+    if item > 10:
+        számláló += 1
+print(számláló, "db 10-nél nagyobb szám van a listában")
+
+print("A lista elemeinek az átlaga:", round(összeg/len(lista), 2))
+
+pos_lista = []
+neg_lista = []
+for item in lista:
+    if item < 0:
+        neg_lista.append(item)
+    if item > 0:
+        pos_lista.append(item)
+print("Pozitív számok:", pos_lista)
+print("Negatív számok:", neg_lista)
