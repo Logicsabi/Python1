098e35991a0d46445c4cce5d2d792deab5a6eb92
Logicsabi <logicsabi@gmail.com>
2025-02-25 18:06:39 +0100

    lista

--- 4ora.py
+++ 4ora.py
@@ -21,3 +21,28 @@ print()
 for i in range(5, 15):
     print(i, end= " ")
 print()
+
+for i in range(2, 100, 10):
+    print(i, end= " ")
+print()
+
+for i in range(100, 10, -10):
+    print(i, end= " ")
+print()
+
+lista = ["Anna", "Beáta", "Cecil", "Dénes", "Elemér", "Ferenc"]
+for item in lista:
+    print(item, end = " ")
+print()
+
+for i in range(len(lista)):
+    print(lista[i], end = " ")
+print()
+
+lista = [11, -4, 7, 1, 6, -5, -12, 0, 11, 14, -8, -9, 2, 23, 11, 10, -4, 0, 18, 4, 8, 1, 1, 11]
+print(lista)
+összeg = 0
+for item in lista:
+    összeg += item
+print("A lista elemeinek az összege:", összeg)
+
