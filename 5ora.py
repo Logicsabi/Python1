d35dc655ad1b7ecd466db1b0600db526190304d7
Logicsabi <logicsabi@gmail.com>
2025-03-04 18:20:52 +0100

    random, guessgame

--- 5ora.py
+++ 5ora.py
@@ -1,3 +1,4 @@
+import random # olyan mint a c#-ban using
 #Elől tesztelős ciklusok (while)
 
 i = 0
@@ -29,4 +30,44 @@ while n > 0:
     a = b
     b = c
     n -= 1
-    print()
+print()
+
+
+# Melyik számra gondoltam?
+num = random.randint(1, 100)
+guessCount = 0
+guess = 0
+while guessCount <= 7 and guess != num:
+    guess = int(input("Tippelj egy egész számot!\n"))
+    if num > guess:
+        print("Ettől nagyobb számra gondoltam.")
+    elif num < guess:
+        print("Ettől kisebb számra gondoltam.")
+    guessCount += 1
+
+if guess == num:
+    print(f"Gratulálok! Sikeresen kitaláltad {guessCount} lépésből, hogy erre a számra gondoltam: {num}")
+else:
+    print(f"Ez most nem jött össze! Erre a számra gondoltam: {num}")
+
+# 1. feladat: Írjunk egy programot ami, ezt írja ki:
+"""
+*
+**
+***
+****
+*****
+******
+*******
+********   (8)
+*******
+******
+*****
+****
+***
+**
+*
+"""
+# 2. feladat: Olvassunk be folyamatosan számokat, amiket összegezzünk, egész addig, amíg 0-t nem írunk be
+#5, 3, 4, 6, 0 -> 18
+
