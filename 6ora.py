5711c4bf95d0adcea28edcadcac30956aabfe869
Logicsabi <logicsabi@gmail.com>
2025-03-11 18:06:39 +0100

    lista3

--- 6ora.py
+++ 6ora.py
@@ -5,7 +5,7 @@ print("A lista elemszáma:", len(nevek))
 
 print(nevek[0])
 print(nevek[3])
-print(nevek[len(nevek - 1)])
+print(nevek[len(nevek) - 1])
 
 print(nevek[-1])
 print(nevek[-4])
@@ -15,3 +15,18 @@ print(nevek[2:5])
 print(nevek[:4])
 
 print(nevek[2:])
+
+print(nevek[:])
+
+nevek2 = nevek
+print(nevek2)
+print(nevek)
+
+nevek = nevek[::-1]
+print(nevek)
+
+i = 0
+while i < len(nevek):
+    print(nevek[i], end= " ")
+    i += 1
+print()
