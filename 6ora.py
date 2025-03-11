72407a2758348b7e50068e69a3349eecaaa1d773
Logicsabi <logicsabi@gmail.com>
2025-03-11 17:48:12 +0100

    lista

---
+++ 6ora.py
@@ -0,0 +1,14 @@
+nevek = ["András", "Béla", "Cecil", "Dénes", "Elemér", "Ferenc"]
+print(nevek)
+print(type(nevek))
+print("A lista elemszáma:", len(nevek))
+
+print(nevek[0])
+print(nevek[3])
+print(nevek[len(nevek - 1)])
+
+print(nevek[-1])
+print(nevek[-4])
+
+
+print(nevek[2:5])
